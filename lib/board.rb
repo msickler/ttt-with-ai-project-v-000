7fddb5c62363fa98a9b0413bcadb0b7c2aa1c85d
Mariana Sickler <marianaleal_93@hotmail.com>
2017-12-20 15:03:24 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -2,54 +2,57 @@ class Board
   attr_accessor :cells
 
   def initialize
-    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-  end
-
-  def display
-  puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
-  puts "-----------"
-  puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
-  puts "-----------"
-  puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
 
   def reset!
-    self.cells.clear
-    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
 
-  def position(user_input)
-  cells[user_input.to_i - 1]
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(n)
+    @cells[n.to_i - 1]
   end
 
   def full?
-  cells.all?{|token| token == "X" || token == "O"}
-  end
-
-  def taken?(user_input)
-     position(user_input) == "X" || position(user_input) == "O"
-  end
-
-  def valid_move?(user_input)
-  if user_input.to_i.between?(1, 9) && (position(user_input) == " " || position(user_input) == "")
-    true
-  else
-    false
-  end
-  end
-
-  def update(user_input, player)
-    cells[user_input.to_i - 1] = player.token
+    # true unless cells.include?(" ")
+    @cells.none? { |board_index| board_index == " " || board_index == "" }
   end
 
   def turn_count
-    count = 0
-  cells.each do |token|
-    if token == "X" || token == "O"
-      count += 1
-  end
-  end
-  return count
+    counter = 0
+    @cells.each do |e|
+      if e == "X" || e == "O"
+        counter += 1
+      end
+    end
+    counter
   end
 
+  def taken?(n)
+    if @cells[n.to_i - 1] == "X" || @cells[n.to_i - 1] == "O"
+      true
+    else
+      false
+    end
+  end
+
+  def valid_move?(n)
+    if n.to_i.between?(1, 9) && !taken?(n)
+      true
+    else
+      false
+    end
+  end
+
+  def update(n, player)
+    @cells[n.to_i - 1 ] = player.token
+  end
 end
